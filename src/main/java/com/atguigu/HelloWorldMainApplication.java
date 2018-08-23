32cdf64f8b3fb9426c018d87628a924e6e6805a9
TowerLeon <top770977@163.com>
2018-08-23 13:27:22 +0800

    20180823 1327

---
+++ src/main/java/com/atguigu/HelloWorldMainApplication.java
@@ -0,0 +1,28 @@
+package com.atguigu; /**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: com.atguigu.HelloWorldMainApplication
+ * Author:   Lenovo
+ * Date:     8/23/2018 1:03 PM
+ * Description: com.atguigu.HelloWorldMainApplication
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈com.atguigu.HelloWorldMainApplication〉
+ *
+ * @author Lenovo
+ * @create 8/23/2018
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class HelloWorldMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldMainApplication.class,args);
+    }
+}
---
+++ src/main/java/com/atguigu/controller/HelloController.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: HelloController
+ * Author:   Lenovo
+ * Date:     8/23/2018 1:08 PM
+ * Description: HelloController
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.atguigu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈HelloController〉
+ *
+ * @author Lenovo
+ * @create 8/23/2018
+ * @since 1.0.0
+ */
+@Controller
+public class HelloController {
+    @ResponseBody
+    @RequestMapping
+    public String hello(){
+        return "Hello World!";
+    }
+}
